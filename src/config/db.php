56143fe1d10f8662ce4a1ff2ce4b8696c6a23e21
Janaka Chathuranga <bjchathuranga@gmail.com>
2015-07-07 00:57:49 +0530

    Added database credentials

--- src/config/db.php
+++ src/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=pris',
     'username' => 'root',
-    'password' => '711246252',
+    'password' => '',
     'charset' => 'utf8',
 ];
