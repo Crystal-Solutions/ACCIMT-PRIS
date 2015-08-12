ac1d61540fee19edbb2e72e9f05730835c238809
Janaka Chathuranga <bjchathuranga@gmail.com>
2015-08-12 22:30:27 +0530

    fixed a bug

--- src/advanced/backend/views/project/index.php
+++ src/advanced/backend/views/project/index.php
@@ -39,7 +39,7 @@ $this->params['breadcrumbs'][] = $this->title;
               'value'=>'parentProject.name',
             ],
 
-             [
+             
 
               [
               'attribute'=>'requested_user_id',
