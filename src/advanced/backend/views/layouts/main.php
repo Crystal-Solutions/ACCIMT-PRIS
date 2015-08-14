b6a2e316e8b0ec42073aa42ae8c59b537f3034dc
Janaka Chathuranga <bjchathuranga@gmail.com>
2015-08-15 02:16:17 +0530

    Removed network link:

--- src/advanced/backend/views/layouts/main.php
+++ src/advanced/backend/views/layouts/main.php
@@ -25,7 +25,6 @@ AppAsset::register($this);
      <script src="<?php echo Yii::$app->request->baseUrl; ?>/ckeditor/ckeditor.js"></script>
 
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
 <?php $this->beginBody() ?>
